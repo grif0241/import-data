81ce192244c7de5dc28f83de91c5b8d4a4f964ef
grif0241 <grif0241@algonquinlive.com>
2021-11-06 15:09:58 -0400

    prints array of objects

---
+++ main.js
@@ -0,0 +1,10 @@
+// Tyler Griffin 
+// grif0241@algonquinlive.com
+// November 6, 2021
+
+const fs = require("fs")
+const data = require('./data')
+
+data.characters.forEach((element) => {
+    console.log(element)
+})
