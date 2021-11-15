dab6d6bb4b3c52f9a63a406954719f6763bf6acf
grif0241 <grif0241@algonquinlive.com>
2021-11-15 11:51:49 -0500

    Filters and sorts an array of objects

--- main.js
+++ main.js
@@ -5,6 +5,23 @@
 const fs = require("fs")
 const data = require('./data')
 
-data.characters.forEach((element) => {
-    console.log(element)
+// FILTER METHOD
+let chars = data.characters.filter((element) => {
+    if(element.race[0] !== "S" && element.race[0] !== "s" ) {
+        return true
+    }
 })
+console.log(chars)
+
+// SORT METHOD
+let sortedChars = chars.sort((a, b) => {
+    if (a.name > b.name) {
+        return 1
+    } else if (b.name  > a.name) {
+        return -1
+    } else {
+        return 0
+    }
+})
+console.log(sortedChars)
+
